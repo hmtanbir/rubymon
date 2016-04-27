9185efba1f26d8dd0d0d2327fadbadf3d4d30c93
hmtanbir <tanbir2025@gmail.com>
2016-04-27 20:47:36 +0600

    signup integrated

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,20 +2,24 @@ class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user,   only: [:edit, :update]
   before_action :admin_user,     only: :destroy
-  layout 'layouts/adminLayout'
+  layout 'layouts/adminLayout',except: :signup
   
+  # get /users/
   def index
     @users = User.paginate(page: params[:page])
   end
   
+  # get  /users/1
   def show
     @user = User.find(params[:id])
   end
   
+# get  /users/new
   def new
     @user = User.new
   end
   
+# post /users/create
   def create
     @user = User.new(user_params)
     if @user.save
@@ -27,9 +31,11 @@ class UsersController < ApplicationController
     end
   end
   
+  # get /users/1/edit
   def edit
   end
   
+  # put /users/1/update
   def update
     if @user.update_attributes(user_params)
       flash[:success] = "Profile updated"
@@ -39,10 +45,19 @@ class UsersController < ApplicationController
     end
   end
   
+  # delete /users/1
   def destroy
     User.find(params[:id]).destroy
     flash[:success] = "User deleted"
     redirect_to users_url
+  end
+
+
+  ## sign up for un-authorized people
+
+# get signup
+  def signup
+    @user = User.new
   end
   
   private
